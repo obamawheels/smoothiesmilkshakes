71805066b7156587c402d8f6ab644d796236c86c
Diego <diego.zamarron0723@outlook.com>
2024-10-27 17:03:26 -0500

    Initial commit

---
+++ src/main/java/net/obama/smoothies/SmoothiesMilkshakesClient.java
@@ -0,0 +1,12 @@
+package net.obama.smoothies;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SmoothiesMilkshakesClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+       
+    }
+
+}
